6e84a7cc7660aa3cc0f6470edefc1a7aca0c5e06
Oskar <o.pulczynski@gmail.com>
2024-09-24 11:13:16 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-'roboto': ["Roboto", sans-serif]
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
